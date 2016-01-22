d53e87dc6bd5ed788034f5cf03d241ec23f179e7
chris berry and Keir Staple <chrisberry321@yahoo.com>
2016-01-21 16:51:35 -0800

    make sure all commits are in

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe ('countUpTo', function(){
 	it ("returns true for a number", function(){
-		expect(countUpTo(30)).to.equal(true);
+		expect(countUpTo(30)).to.equal(alert);
 	});
 });
 
@@ -21,52 +21,3 @@ describe ('indexOutput', function(){
 		expect(indexOutput(10, 5)).to.eql([5, 10]);
 	});
 });
-
-
-
-
-
-
-// 	describe ('countUpTo' && 'countUpBy', function() {
-// 		it("is false if countUpBy is greater than countUpTo", function() {
-// 				expect(countUpBy, countUpTo(32, 30)).to.equal(false);
-// 			});
-// 		it("is true if countUpBy is less than or equal to countUpTo", function() {
-// 			expect(countUpBy, countUpTo(3, 3)).to.equal(true)
-// 		});
-// });
-
-
-
-
-
-
-
-
-
-
-// describe ('leapYear', function() {
-// 	it("is false for a year that is not divisible by 4, 100, or 400", function() {
-// 		expect(leapYear(1993)).to.equal(false);
-// 	});
-//
-// 	it ("is true for years divisible by 4", function() {
-// 		expect(leapYear(2004)).to.equal(true);
-// 	});
-//
-// 	it ("is false for years divisible by 100", function() {
-// 		expect (leapYear(1900)).to.equal(false);
-// 	});
-//
-// 	it ("is true for years divisible by 400", function() {
-// 		expect (leapYear(2000)).to.equal(true);
-// 	});
-//
-// 	it ("is false for blank input", function() {
-// 		expect (leapYear(NaN)).to.equal(false);
-// 	});
-//
-// 	it ("is false for negavtive integers", function() {
-// 		expect (leapYear(-400)).to.equal(false);
-// 	});
-// });
